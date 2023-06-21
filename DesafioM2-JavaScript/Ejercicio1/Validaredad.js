849553d26d4d1a4a1a91fc4ef4fc51520c1cdef1
Natalia <rsupponatalia@gmail.com>
2023-06-21 14:58:10 -0600

    CorreccionEjerc1-M2-Js

--- DesafioM2-JavaScript/Ejercicio1/Validaredad.js
+++ DesafioM2-JavaScript/Ejercicio1/Validaredad.js
@@ -1,12 +1,23 @@
-edad = 17
+const readline = require('readline');
+let interfazCaptura = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
-console.log("Ingrese su edad :"+ edad)
-if (edad >= 18) {
-    console.log("Bienvenido");
-}
-else {
-    console.log("Acceso denegado");
-}
+interfazCaptura.question('Ingrese su edad:', function (respuesta) {
+    edad = respuesta
+    if (edad >= 18) {
+        console.log("Bienvenido");
+    }
+    else {
+        console.log("Acceso denegado");
+    }
+
+    interfazCaptura.close()
+})
+
+
+
 
 
 
