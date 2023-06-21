02da6927848ec8756ece8faef27d6545458dbfb8
Natalia <rsupponatalia@gmail.com>
2023-06-21 13:37:10 -0600

    DesafioM1-Js-Ejerc2

---
+++ DesafioM1-JavaScript/Ejercicio2/PresupPersonal.js
@@ -0,0 +1,11 @@
+ingresoTotal= 50000
+
+
+gastosNecesarios=ingresoTotal*0.5
+gastosOpcionales=ingresoTotal*0.3
+ahorroInversion =ingresoTotal*0.2
+console.log("El ingreso total es :"+ingresoTotal)
+console.log("Gastos Necesarios:"+gastosNecesarios)
+console.log("Gastos Opcionales:"+ gastosOpcionales)
+console.log("Ahorro e Inversión:"+ ahorroInversion)
+
