c2b3efdd90f21712c1345023c2c2273b2fa9a89a
Natalia <rsupponatalia@gmail.com>
2023-06-21 15:14:08 -0600

    Ejerc2-M2-JS

--- DesafioM2-JavaScript/Ejercicio2/InicioSesion.js
+++ DesafioM2-JavaScript/Ejercicio2/InicioSesion.js
@@ -1,3 +1,13 @@
 
+usuario= 'Admin'
+clave= '1235'
 
+console.log("Ingrese su usuario y clave")
+
+if (usuario=='Admin'&&clave=='1234') {
+    console.log("Bienvenido");
+}
+else {
+    console.log("Acceso denegado");
+}
 
