850840894b4b5850327c9531f69ffb6620999fb9
Natalia <rsupponatalia@gmail.com>
2023-06-29 08:06:17 -0600

    DesafioM5-jsEjercicio1

---
+++ DesafioM5-JavaScript/Ejercicio1/PE-Calculo.js
@@ -0,0 +1,15 @@
+
+costoFijo= 15
+precioVenta=50
+costoVariable=6
+
+puntoEquilibrio=costoFijo / (precioVenta - costoVariable)
+console.log("Costo fijo:"+costoFijo)
+console.log("Precio de venta:"+ precioVenta)
+console.log("Costo Variable:"+ costoVariable)
+console.log(puntoEquilibrio)
+
+
+
+
+
